d4bc329e9d254635443e15450bde4993830f5497
khairul_mubin <khairul.mubin@orbitax.com>
2023-06-08 19:13:46 +0600

    simple changes

--- Chat.Api/IdentityModule/CommandHandlers/LoginCommandHandler.cs
+++ Chat.Api/IdentityModule/CommandHandlers/LoginCommandHandler.cs
@@ -26,10 +26,14 @@ namespace Chat.Api.IdentityModule.CommandHandlers
         public override async Task<CommandResponse> OnHandleAsync(LoginCommand command)
         {
             var response = command.CreateResponse();
-            var user = await _userRepository.GetUserAsync(command.Email, command.Password);
+            var user = await _userRepository.GetUserByEmailAsync(command.Email);
             if (user == null) 
             {
-                throw new Exception("Email or Password error!!");
+                throw new Exception("Email error!!");
+            }
+            if (user.Password != command.Password)
+            {
+                throw new Exception("Password error!!");
             }
             var userProfile = user.ToUserProfile();
             var token = await _tokenService.CreateTokenAsync(userProfile, command.AppId);
